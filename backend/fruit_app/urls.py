9e99f74368b88a3ddde8345313a307583a3beb18
Emil Marsal <emilmarsal@icloud.com>
2026-03-16 16:09:25 +0100

    Add fruit images and info page, update fruit list rendering

--- backend/fruit_app/urls.py
+++ backend/fruit_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import send_fruits
+from .views import send_fruits, info
 
 
 urlpatterns = [
     path('', send_fruits),
+    path('info/', info, name="info"),
 ]
--- backend/fruit_app/views.py
+++ backend/fruit_app/views.py
@@ -18,10 +18,34 @@ from django.http import JsonResponse
 def send_fruits(request):
 
     fruits = [
-        {"name": "Apfel", "gewicht": 150, "farbe": "rot"},
-        {"name": "Banane", "gewicht": 120, "farbe": "gelb"},
-        {"name": "Orange", "gewicht": 180, "farbe": "orange"},
-        {"name": "Kiwi", "gewicht": 90, "farbe": "braun"},
-        {"name": "Traube", "gewicht": 5, "farbe": "lila"}
+        {"name": "Apfel",
+         "gewicht": 150,
+         "farbe": "rot",
+         "img": "fruit_app/img/apfel.jpg",
+         "is_ordered": True},
+        {"name": "Banane",
+         "gewicht": 120,
+         "farbe": "gelb",
+         "img": "fruit_app/img/banane.jpg",
+         "is_ordered": True},
+        {"name": "Orange",
+         "gewicht": 180,
+         "farbe": "orange",
+         "img": "fruit_app/img/orange.jpg",
+         "is_ordered": False},
+        {"name": "Kiwi",
+         "gewicht": 90,
+         "farbe": "braun",
+         "img": "fruit_app/img/kiwi.jpg",
+         "is_ordered": False},
+        {"name": "Traube",
+         "gewicht": 5,
+         "farbe": "lila",
+         "img": "fruit_app/img/traube.jpg",
+         "is_ordered": True}
     ]
     return render(request, "fruit_app/fruitlist.html", {"fruits": fruits})
+
+
+def info(request):
+    return render(request, "fruit_app/info.html")
